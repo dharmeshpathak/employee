a711d9e69376418ae512a0bf694eb72bbf8655d4
dharmesh <dharmeshpathak1999@gmail.com>
2022-03-11 16:24:23 +0530

    unique email funtionality added

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,131 +1,142 @@
-import { Paper, Typography, Box, Button,TextField } from '@mui/material';
-import '../../styles/inputfield.css';
-import instance from '../../api'
-import NewNavbar from '../Navbar/NewNavbar'
-import * as yup from 'yup';
-import React from 'react';
-import { useFormik } from 'formik';
-
+import { Paper, Typography, Box, Button, TextField } from "@mui/material";
+import "../../styles/inputfield.css";
+import instance from "../../api";
+import NewNavbar from "../Navbar/NewNavbar";
+import * as yup from "yup";
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import InfoIcon from "@mui/icons-material/Info";
 const validationSchema = yup.object({
   email: yup
-    .string('Enter your email')
-    .email('Enter a valid email')
-    .required('Email is required'),
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
   password: yup
-    .string('Enter your password')
-    .min(8, 'Password should be of minimum 8 characters length')
-    .required('Password is required'),
-    cpassword: yup.string()
-     .oneOf([yup.ref('password'), null], 'Passwords must match')
-
+    .string("Enter your password")
+    .min(4, "Password should be of minimum 8 characters length")
+    .required("Password is required"),
+  cpassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match"),
 });
-function SignUp({login,setUpLogin}) {
+function SignUp({ login, setUpLogin }) {
+  const [match, setmatch] = useState(false);
 
   const formik = useFormik({
     initialValues: {
-      email: 'foobar@example.com',
-      password: 'foobar',
-      cpassword:'foobar'
+      email: "",
+      password: "",
+      cpassword: "",
     },
     validationSchema: validationSchema,
-    onSubmit: async(values) => {
-      const res = await instance.post(`/users`, {
-        email: values.email,
-        password: values.password,
-      });
-      if (res.status === 201) {
-        console.log('user Added');
-        values.email='';
+    onSubmit: async (values) => {
+      const { data } = await instance.get("/users");
+      const user = data.filter((emp) => emp.username === values.email);
+      if (user.length) {
+        setmatch(true);
+        return;
+      } else {
+        const res = await instance.post(`/users`, {
+          username: values.email,
+          password: values.password,
+        });
+        if (res.status === 201) {
+          console.log("user Added");
+          values.email = "";
 
-        values.password='';
-        values.cpassword='';
-        
+          values.password = "";
+          values.cpassword = "";
+        }
       }
     },
   });
 
-
- 
-
   return (
-    <NewNavbar login={login} setUpLogin = {setUpLogin}>
-    <form onSubmit={formik.handleSubmit} >
-    <Box
-      elevation={1}
-      component={Paper}
-      p={4}
-      display='flex'
-      flexDirection={'column'}
-      justifyContent={'center'}
-      alignItems={'center'}
-      maxWidth={700}
-      margin={'auto'}
-      mt={4}
-      gap={2}
-    >
-    
-      <Typography
-        variant='h4'
-        component='h4'
-        fontWeight={700}
-        m={3}
-        textAlign={'center'}
-      >
-        SignUp
-      </Typography>
-      <TextField
-        name='email'
-        width={300}
-        placeholder='enter email'
-        className='inputField'
-        type='email'
-        value={formik.values.email}
-        onChange={formik.handleChange}
-        error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        
-        
-      />
+    <NewNavbar login={login} setUpLogin={setUpLogin}>
+      <form onSubmit={formik.handleSubmit}>
+        <Box
+          elevation={1}
+          component={Paper}
+          p={4}
+          display="flex"
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          maxWidth={700}
+          margin={"auto"}
+          mt={4}
+          gap={2}
+          position='relative'
+        >
+          {match && (
+            <span
+              style={{
+                color: "red",
+                tranformText: "capitalise",
+                backgroundColor: "rgba(255,0,0,0.1)",
+                position: "absolute",
+                width: "100%",
+                top: "0",
+                left: "0",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "10px 0",
+              }}
+            >
+              <InfoIcon /> &nbsp; User Already Exist
+            </span>
+          )}
+          <Typography
+            variant="h4"
+            component="h4"
+            fontWeight={700}
+            m={3}
+            textAlign={"center"}
+          >
+            SignUp
+          </Typography>
+          <TextField
+            name="email"
+            width={300}
+            placeholder="enter email"
+            className="inputField"
+            type="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
 
-      <TextField
-        name='password'
-        width={300}
-        placeholder='Enter password'
-        className='inputField'
-        type={'password'}
-        value={formik.values.password}
-        onChange={formik.handleChange}
-        error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
-        
-        
-      />
-      
-      <TextField
-      width={300}
-        name='cpassword'
-        placeholder='Confirm Password'
-        className='inputField'
-        type={'password'}
-        value={formik.values.cpassword}
-        onChange={formik.handleChange}
-        error={formik.touched.cpassword && Boolean(formik.errors.cpassword)}
-          helperText={formik.touched.cpassword && formik.errors.cpassword}
-        
-        
-      />
+          <TextField
+            name="password"
+            width="300"
+            placeholder="Enter password"
+            className="inputField"
+            type={"password"}
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+          />
 
+          <TextField
+            width={300}
+            name="cpassword"
+            placeholder="Confirm Password"
+            className="inputField"
+            type={"password"}
+            value={formik.values.cpassword}
+            onChange={formik.handleChange}
+            error={formik.touched.cpassword && Boolean(formik.errors.cpassword)}
+            helperText={formik.touched.cpassword && formik.errors.cpassword}
+          />
 
-
-
-      <Button variant='outlined' type='submit'>
-        SignUp
-      </Button>
-      
-    </Box>
-    </form>
-
-
+          <Button variant="outlined" type="submit">
+            SignUp
+          </Button>
+        </Box>
+      </form>
     </NewNavbar>
   );
 }
