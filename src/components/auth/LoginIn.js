9d467dc9f70cfe983960b9d9570e34e630204af0
dharmesh <dharmeshpathak1999@gmail.com>
2022-03-11 15:21:23 +0530

    formik added to login and signup page

--- src/components/auth/LoginIn.js
+++ src/components/auth/LoginIn.js
@@ -1,111 +1,133 @@
-import { useEffect, useState } from 'react';
-import { Paper, Typography, Box, Button } from '@mui/material';
-import '../../styles/inputfield.css';
-import instance from '../../api'
-import { Link, useNavigate } from 'react-router-dom';
-import NewNavbar from '../Navbar/NewNavbar'
+import { useState, useEffect } from "react";
+import { Paper, Typography, Box, Button, TextField } from "@mui/material";
+import "../../styles/inputfield.css";
+import instance from "../../api";
+import { useNavigate } from "react-router-dom";
+import NewNavbar from "../Navbar/NewNavbar";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import InfoIcon from '@mui/icons-material/Info';
+const validationSchema = yup.object({
+  email: yup.string("Enter your email").required("Email is required"),
+  password: yup.string("Enter your password").required("Password is required"),
+});
 const LoginIn = ({ setUpLogin, login }) => {
   let navigate = useNavigate();
-  const [employee, setEmployee] = useState({
-    username: '',
-    password: '',
+  const [match, setmatch] = useState(true);
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      const { data, status } = await instance.get(`/users`);
+      console.log(data);
+
+      if (status === 200) {
+        const user = data.filter((emp) => {
+          return (
+            emp.username === values.email && emp.password === values.password
+          );
+        });
+        if (user.length !== 0) {
+          setmatch(true);
+          console.log(user);
+
+          localStorage.setItem("userItem", JSON.stringify(user[0]));
+
+          navigate("/");
+        } else {
+          setmatch(false);
+
+          return;
+        }
+      }
+    },
   });
 
-  const [match, setmatch] = useState(true);
-  const handleChange = (e) => {
-    setEmployee(() => {
-      return { ...employee, [e.target.name]: e.target.value };
-    });
-  };
-
-  const loginEmp = async () => {
-    const { data, status } = await instance.get(`/users`);
-
-    if (status === 200) {
-      const user = data.filter((emp) => {
-        return (
-          emp.username === employee.username && emp.password === employee.password
-        );
-      });
-      if (user.length !== 0) {
-        setmatch(true);
-
-        localStorage.setItem('userItem', JSON.stringify(user[0]));
-
-        navigate('/');
-      } else {
-        setmatch(false);
-        localStorage.setItem('userItem', null);
-      }
-    }
-  };
   useEffect(() => {
     setUpLogin();
-    console.log('login = ', login);
-    if (login) navigate('/');
+    console.log("login = ", login);
+    if (login) navigate("/");
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [login]);
 
   return (
-    <NewNavbar login={login} setUpLogin={setUpLogin} >
-    <Box
-      elevation={1}
-      component={Paper}
-      p={4}
-      display='flex'
-      flexDirection={'column'}
-      justifyContent={'center'}
-      alignItems={'center'}
-      maxWidth={700}
-      margin={'auto'}
-      mt={4}
-    >
-      <Typography
-        variant='h4'
-        component='h4'
-        fontWeight={700}
-        m={3}
-        textAlign={'center'}
-      >
-        LogIn
-      </Typography>
-      {!match && <label style={{ color: 'red' }}>Invalid Credentials</label>}
-      <input
-        name='username'
-        placeholder='Enter usename'
-        className='inputField'
-        type={'text'}
-        value={employee.username}
-        onChange={handleChange}
-        required
-      />
+    <NewNavbar login={login} setUpLogin={setUpLogin}>
+      <form onSubmit={formik.handleSubmit}>
+        <Box
+          elevation={1}
+          component={Paper}
+          p={4}
+          display="flex"
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          maxWidth={700}
+          margin={"auto"}
+          mt={4}
+          gap={2}
+          position={'relative'}
+        >
+          {!match && (
+            <span
+              style={{
+                color: "red",
+                tranformText: "capitalise",
+                backgroundColor:'rgba(255,0,0,0.1)',
+                position:"absolute",
+                width:"100%",
+                top:'0',
+                left:'0',
+                display:'flex',
+                justifyContent:"center",
+                alignItems:"center",
+                padding:"10px 0"
+              }}
+            ><InfoIcon /> &nbsp;
+              Invalid credential
+            </span>
+          )}
 
-      <input
-        name='password'
-        placeholder='Enter password'
-        className='inputField'
-        type={'password'}
-        value={employee.password}
-        onChange={handleChange}
-        required
-      />
+          <Typography
+            variant="h4"
+            component="h4"
+            fontWeight={700}
+            m={3}
+            textAlign={"center"}
+          >
+            LogIn
+          </Typography>
 
-      <Button variant='outlined' onClick={loginEmp}>
-        Login
-      </Button>
-      <Link
-        to='/signup'
-        style={{
-          textDecoration: 'none',
-          padding: '5px 20px',
-          border: '2px solid skyblue',
-          marginTop: '10px',
-        }}
-      >
-        SignUp
-      </Link>
-    </Box>
+          <TextField
+            name="email"
+            width={300}
+            placeholder="enter email"
+            className="inputField"
+            type={"text"}
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+
+          <TextField
+            name="password"
+            placeholder="Enter password"
+            className="inputField"
+            type={"text"}
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            required
+          />
+
+          <Button variant="outlined" type="submit">
+            Login
+          </Button>
+        </Box>
+      </form>
     </NewNavbar>
   );
 };
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,47 +1,55 @@
-import { useState } from 'react';
-import { Paper, Typography, Box, Button } from '@mui/material';
+import { Paper, Typography, Box, Button,TextField } from '@mui/material';
 import '../../styles/inputfield.css';
 import instance from '../../api'
 import NewNavbar from '../Navbar/NewNavbar'
+import * as yup from 'yup';
+import React from 'react';
+import { useFormik } from 'formik';
+
+const validationSchema = yup.object({
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string('Enter your password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+    cpassword: yup.string()
+     .oneOf([yup.ref('password'), null], 'Passwords must match')
+
+});
 function SignUp({login,setUpLogin}) {
-  const [employee, setEmployee] = useState({
-    username: '',
-    password: '',
-    cpassword: '',
-  });
-  const [match, setmatch] = useState(true);
-  const handleChange = (e) => {
-    setEmployee(() => {
-      return { ...employee, [e.target.name]: e.target.value };
-    });
-  };
 
-  const addEmployee = async () => {
-    if (employee.cpassword !== employee.password) {
-      setmatch(false);
-      
-      return;
-    } else if (employee.cpassword === employee.password) {
-      setmatch(true);
-     
-    }
-
-    const res = await instance.post(`/users`, {
-      username: employee.username,
-      password: employee.password,
-    });
-    if (res.status === 201) {
-      console.log('user Added');
-      setEmployee({
-        username: '',
-        password: '',
-        cpassword: '',
+  const formik = useFormik({
+    initialValues: {
+      email: 'foobar@example.com',
+      password: 'foobar',
+      cpassword:'foobar'
+    },
+    validationSchema: validationSchema,
+    onSubmit: async(values) => {
+      const res = await instance.post(`/users`, {
+        email: values.email,
+        password: values.password,
       });
-    }
-  };
+      if (res.status === 201) {
+        console.log('user Added');
+        values.email='';
+
+        values.password='';
+        values.cpassword='';
+        
+      }
+    },
+  });
+
+
+ 
 
   return (
     <NewNavbar login={login} setUpLogin = {setUpLogin}>
+    <form onSubmit={formik.handleSubmit} >
     <Box
       elevation={1}
       component={Paper}
@@ -53,7 +61,9 @@ function SignUp({login,setUpLogin}) {
       maxWidth={700}
       margin={'auto'}
       mt={4}
+      gap={2}
     >
+    
       <Typography
         variant='h4'
         component='h4'
@@ -63,39 +73,59 @@ function SignUp({login,setUpLogin}) {
       >
         SignUp
       </Typography>
-      <input
-        name='username'
-        placeholder='Enter usename'
+      <TextField
+        name='email'
+        width={300}
+        placeholder='enter email'
         className='inputField'
-        type={'text'}
-        value={employee.username}
-        onChange={handleChange}
-        required
+        type='email'
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        
+        
       />
 
-      <input
+      <TextField
         name='password'
+        width={300}
         placeholder='Enter password'
         className='inputField'
         type={'password'}
-        value={employee.password}
-        onChange={handleChange}
-        required
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        
+        
       />
-      {!match && <label style={{ color: 'red' }}>Passwords do not match</label>}
-      <input
+      
+      <TextField
+      width={300}
         name='cpassword'
         placeholder='Confirm Password'
         className='inputField'
         type={'password'}
-        value={employee.cpassword}
-        onChange={handleChange}
-        required
+        value={formik.values.cpassword}
+        onChange={formik.handleChange}
+        error={formik.touched.cpassword && Boolean(formik.errors.cpassword)}
+          helperText={formik.touched.cpassword && formik.errors.cpassword}
+        
+        
       />
-      <Button variant='outlined' onClick={addEmployee}>
+
+
+
+
+      <Button variant='outlined' type='submit'>
         SignUp
       </Button>
+      
     </Box>
+    </form>
+
+
     </NewNavbar>
   );
 }
