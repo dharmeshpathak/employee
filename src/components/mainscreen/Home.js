cda92a9cf1a1159cfe2437f3c62fe3d03c8ee303
dharmesh <dharmeshpathak1999@gmail.com>
2022-03-15 14:27:49 +0530

    Option menu added for actions, Checkbox position changed, bulk delete button updated

--- src/components/mainscreen/Home.js
+++ src/components/mainscreen/Home.js
@@ -1,188 +1,219 @@
-import { useCallback, useEffect, useState } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import { Paper, Checkbox, Button, ButtonGroup ,Box,Divider} from '@mui/material';
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
-import instance from '../../api/index';
-import Toggle from '../mainscreen/Toggle'
-import NewNavbar from '../Navbar/NewNavbar'
-import CardElem from '../mainscreen/CardElem'
-import Modl from '../mainscreen/Modal/Modl'
-import "../../styles/table.css"
+import { useCallback, useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import {
+  Paper,
+  Checkbox,
+  Button,
+  Box,
+  Divider,
+} from "@mui/material";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import instance from "../../api/index";
+import Toggle from "../mainscreen/Toggle";
+import NewNavbar from "../Navbar/NewNavbar";
+import CardElem from "../mainscreen/CardElem";
+import Modl from "../mainscreen/Modal/Modl";
+import OptionMenu from "../mainscreen/OptionMenu";
+import "../../styles/table.css";
 function Home({ login, setUpLogin }) {
- 
   const [employee, setEmployees] = useState([]);
-  
- const [delEmp, setDelEmp] = useState([]);
- const [gridView, setgridView] = useState(false)
- const [editId, seteditId] = useState("")
 
- const [open, setOpen] = useState(false);
+  const [delEmp, setDelEmp] = useState([]);
+  const [gridView, setgridView] = useState(false);
+  const [editId, seteditId] = useState("");
+
+  const [open, setOpen] = useState(false);
   const handleOpen = (id) => {
-    
-    seteditId(id)
-    setOpen(true);}
-  const handleClose = async() =>{ 
-  setOpen(false);}
- const setView = (value)=>{
-   if(value==='Table')
-   setgridView(false)
-   else if(value ==='Grid')
-   setgridView(true)
-  
- }
-
+    seteditId(id);
+    setOpen(true);
+  };
+  const handleClose = async () => {
+    setOpen(false);
+  };
+  const setView = (value) => {
+    if (value === "Table") setgridView(false);
+    else if (value === "Grid") setgridView(true);
+  };
 
   const getEmployee = useCallback(async () => {
-    const {data} = await instance.get(`/employees`);
-   
+    const { data } = await instance.get(`/employees`);
+    console.log(data);
     setEmployees(data);
-   
   }, []);
-
-  
 
   const deleteEmployee = async (id) => {
     const res = await instance.delete(`/employees/${id}`);
     getEmployee();
-   
+
     if (res.status !== 404) {
-      console.log('employee deleted');
+      console.log("employee deleted");
     }
   };
   const handleOnChange = (id) => {
-
-    if(delEmp.includes(id)){
-      setDelEmp(delEmp.filter((item)=>item !== id)) 
-    }else{
-      setDelEmp([...delEmp,id])
-      
+    if (delEmp.includes(id)) {
+      setDelEmp(delEmp.filter((item) => item !== id));
+    } else {
+      setDelEmp([...delEmp, id]);
     }
-    console.log(delEmp)
-    
+    console.log(delEmp);
   };
 
   const bulkDelete = () => {
-      delEmp.map(
-      async (ids, index) =>  {
-        await instance.delete(`/employees/${ids}`)
-        getEmployee();
-  }
-    );
+    delEmp.map(async (ids, index) => {
+      await instance.delete(`/employees/${ids}`);
+      getEmployee();
+    });
     setDelEmp([]);
-    
-    
   };
- 
+
   useEffect(() => {
     getEmployee();
-  }, [getEmployee,open]);
+  }, [getEmployee, open]);
 
- useEffect(()=>{
-console.log(delEmp);
- },[delEmp])
-
+  useEffect(() => {
+    console.log(delEmp);
+  }, [delEmp]);
 
   return (
-    <NewNavbar login={login} setUpLogin={setUpLogin} >
-    <div   >
-    <div style={{display:"flex",justifyContent:"center" ,margin:'20px'}}>
+    <NewNavbar login={login} setUpLogin={setUpLogin}>
+      <div>
+        <div
+          style={{ display: "flex", justifyContent: "center", margin: "20px" }}
+        >
+          <Modl
+            open={open}
+            handleClose={handleClose}
+            login={login}
+            setUpLogin={setUpLogin}
+            editId={editId}
+          />
 
-    <Modl open = {open} handleClose = {handleClose}  login={login} setUpLogin={setUpLogin} editId = {editId}  />
-
-    <Toggle setView={setView} /></div>
-    <Divider/>
-      { !gridView && <Paper className='container'  style={{margin :'50px auto 0 auto' ,width:'850px'}}>
-        <Table    >
-          <TableHead style={{ backgroundColor: '#00E' }}>
-            <TableRow>
-              <TableCell style={{ color: 'white' }}>ID</TableCell>
-              <TableCell style={{ color: 'white' }} align='left'>
-                Name
-              </TableCell>
-              <TableCell style={{ color: 'white' }} align='left'>
-                Email
-              </TableCell>
-              <TableCell style={{ color: 'white' }} align='left'>
-                DOB
-              </TableCell>
-              <TableCell style={{ color: 'white' }} align='left'>
-                Phone
-              </TableCell>
-              <TableCell style={{ color: 'white' }} align='center'>
-                Action
-              </TableCell>
-              <TableCell style={{ color: 'white' }} align='center'>
-                <Button onClick={bulkDelete}>
-                  <DeleteOutlineIcon style={{ color: 'white' }} />
-                </Button>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {employee.map((row, index) => (
-              <TableRow
-                key={row.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component='th' scope='row'>
-                  {row.id}
-                </TableCell>
-                <TableCell align='left'>{row.name}</TableCell>
-                <TableCell align='left'>{row.email}</TableCell>
-                <TableCell align='left'>{row.dob}</TableCell>
-                <TableCell align='left'>{row.phone}</TableCell>
-                <TableCell align='center'>
-                  <ButtonGroup
-                    variant='contained'
-                    aria-label='outlined primary button group'
+          <Toggle setView={setView} />
+        </div>
+        <Divider />
+        {!gridView && (
+          <Paper
+            className="container"
+            style={{ margin: "50px auto 0 auto", width: "850px", zIndex: 1 }}
+          >
+            <Table>
+              <TableHead style={{ backgroundColor: "#00E" }}>
+                <TableRow>
+                <TableCell style={{ color: "white" }} align="center">
+                    <Button  onClick={bulkDelete} disabled={delEmp.length===0}>
+                    <DeleteOutlineIcon style={{ color: "white" }}/> 
+                    </Button>
+                  </TableCell>
+                  <TableCell style={{ color: "white" }}>ID</TableCell>
+                  <TableCell style={{ color: "white" }} align="left">
+                    Name
+                  </TableCell>
+                  <TableCell style={{ color: "white" }} align="left">
+                    Email
+                  </TableCell>
+                  <TableCell style={{ color: "white" }} align="left">
+                    DOB
+                  </TableCell>
+                  <TableCell style={{ color: "white" }} align="left">
+                    Phone
+                  </TableCell>
+                  <TableCell style={{ color: "white" }} align="center">
+                    Action
+                  </TableCell>
+                  
+                  
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {employee.map((row, index) => (
+                  <TableRow
+                    key={row.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                   >
-                    <Button
-                      onClick={() => deleteEmployee(row.id)}
-                      variant='outlined'
-                    >
-                      <DeleteOutlineIcon />
-                    </Button>
-                    <Button variant='outlined' onClick={()=>handleOpen(row.id)}>
-                      
-                        <EditOutlinedIcon />
-                     
-                    </Button>
-                  </ButtonGroup>
-                </TableCell>
-                <TableCell align='center'>
-                  <Checkbox
-                   
-                    id={`custom-checkbox-${index}`}
-                    name={row.name}
-                    value={row.id}
-                   
-                    onChange={() => handleOnChange(row.id)}
-                  />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        
-      </Paper>}
+                  <TableCell align="center">
+                      <Checkbox
+                        id={`custom-checkbox-${index}`}
+                        name={row.name}
+                        value={row.id}
+                        onChange={() => handleOnChange(row.id)}
+                      />
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                      {row.id}
+                    </TableCell>
+                    <TableCell align="left">{row.name}</TableCell>
+                    <TableCell align="left">{row.email}</TableCell>
+                    <TableCell align="left">{row.dob}</TableCell>
+                    <TableCell align="left">{row.phone}</TableCell>
 
+                    
+                    <TableCell>
+                      <OptionMenu
+                        handleOpen={handleOpen}
+                        deleteEmployee={deleteEmployee}
+                        id={row.id}
+                        handleClose={handleClose}
+                      />
+                    </TableCell>
+                    
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Paper>
+        )}
 
-      {gridView && <Box   style={{margin :'50px auto 0 auto' ,display:"flex",justifyContent:"space-evenly",flexDirection:'column',flexWrap:'wrap'}}>
+        {gridView && (
+          <Box
+            style={{
+              margin: "50px auto 0 auto",
+              display: "flex",
+              justifyContent: "space-evenly",
+              flexDirection: "column",
+              flexWrap: "wrap",
+            }}
+          >
+            
+              <Button
+                style={{ placeSelf: "flex-end", marginRight: "50px" }}
+                variant="outlined"
+                onClick={bulkDelete}
+                disabled={delEmp.length === 0}
+              >
+                Delete Selected
+              </Button>
+           
 
-     {(delEmp.length>0) && <Button style ={{placeSelf:'flex-end',marginRight:'50px'}} variant= 'outlined' onClick ={bulkDelete}>Delete Selected</Button>}
-
-      <Box style={{margin :'50px auto 0 auto' ,display:"flex",justifyContent:"space-evenly",flexDirection:'row',flexWrap:'wrap', gap:'20px',maxWidth:"940px" }}>
-
-      {employee.map((emp)=><CardElem key = {`card ${emp.id}`} emp = {emp} deleteEmployee={deleteEmployee} handleOnChange={handleOnChange} open = {open} handleClose = {handleClose} handleOpen={handleOpen} />
-      )}</Box>
-        
-      </Box>}
-    </div>
+            <Box
+              style={{
+                margin: "50px auto 0 auto",
+                display: "flex",
+                justifyContent: "space-evenly",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                gap: "20px",
+                maxWidth: "940px",
+              }}
+            >
+              {employee.map((emp) => (
+                <CardElem
+                  key={`card ${emp.id}`}
+                  emp={emp}
+                  deleteEmployee={deleteEmployee}
+                  handleOnChange={handleOnChange}
+                  open={open}
+                  handleClose={handleClose}
+                  handleOpen={handleOpen}
+                />
+              ))}
+            </Box>
+          </Box>
+        )}
+      </div>
     </NewNavbar>
   );
 }
---
+++ src/components/mainscreen/OptionMenu.js
@@ -0,0 +1,113 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+const options = [
+  <EditOutlinedIcon style={{ margin: "2px 4px" }} />,
+  <DeleteOutlineIcon style={{ margin: "2px 4px" }} />,
+];
+
+export default function SplitButton({
+  deleteEmployee,
+  handleOpen,
+  id,
+  handleClose,
+}) {
+  const [openOption, setOpenOption] = React.useState(false);
+  const anchorRef = React.useRef(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
+    setOpenOption(false);
+  };
+
+  const handleToggle = () => {
+    setOpenOption((prevOpen) => !prevOpen);
+  };
+
+  const handleCloseoption = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpenOption(false);
+  };
+
+  return (
+    <>
+      <ButtonGroup
+        variant="contained"
+        ref={anchorRef}
+        aria-label="outlined primary split button"
+        
+
+      >
+        <Button
+        variant="outlined"
+        style={{backgroundColor:"white",
+        }}
+          onClick={() => {
+            selectedIndex === 0 ? handleOpen(id) : deleteEmployee(id);
+          }}
+        >
+          {options[selectedIndex]}
+        </Button>
+        <Button
+         variant="outlined"
+        style={{backgroundColor:"white",
+        }}
+          size="small"
+          aria-controls={openOption ? "split-button-menu" : undefined}
+          aria-expanded={openOption ? "true" : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          onClick={handleToggle}
+        >
+          <ArrowDropDownIcon />
+        </Button>
+      </ButtonGroup>
+      <Popper
+        open={openOption}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+        style={{ zIndex: 5 }}
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin: placement === "center top",
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleCloseoption}>
+                <MenuList id="split-button-menu">
+                  {options.map((option, index) => (
+                    <MenuItem
+                      key={`${option} ${index}`}
+                      selected={index === selectedIndex}
+                      onClick={(event) => handleMenuItemClick(event, index)}
+                    >
+                      {option}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </>
+  );
+}
